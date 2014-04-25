709d35038d5b0e264858ce8c79b6891a63631ecf
Powderpat <wpsawrey@gmail.com>
2014-04-25 16:59:09 -0700

    sea urchin

--- draw.py
+++ draw.py
@@ -4,16 +4,14 @@ import turtle
 
 carlos = turtle.Turtle()
 
-n = 6
+n = 50
 length = 100
-angle = 180 - 180 * (n - 2) / n
-#numbers = range(0, n)
-colors = [ "red", "yellow", "orange", "green", "magenta", "blue"]
+angle = 170
+numbers = range(0, n)
 
-yash = turtle.Turtle()
 
-for color in colors:
-	carlos.color(color)
+for number in numbers:
+	
 	carlos.forward(length)
 	carlos.left(angle)
 	
