0677f3d8d207dee95f79c76763f4999b599f5c96
Powderpat <wpsawrey@gmail.com>
2014-04-25 16:50:05 -0700

    lists range and colors

--- draw.py
+++ draw.py
@@ -4,11 +4,16 @@ import turtle
 
 carlos = turtle.Turtle()
 
-n = 5
-length = 50
+n = 6
+length = 100
 angle = 180 - 180 * (n - 2) / n
-numbers = range(0, n)
+#numbers = range(0, n)
+colors = [ "red", "yellow", "orange", "green", "magenta", "blue"]
 
-for number in numbers:
+yash = turtle.Turtle()
+
+for color in colors:
+	carlos.color(color)
 	carlos.forward(length)
 	carlos.left(angle)
+	
