7e8eaa87ffe83115af4b6bb0bd6c5dc272c3278a
Powderpat <wpsawrey@gmail.com>
2014-04-25 17:38:28 -0700

    pythonroom update

--- draw.py
+++ draw.py
@@ -5,19 +5,18 @@ import turtle
 carlos = turtle.Turtle()
 
 n = 50
-t = 30
-length = 1	
-angle = 30
-numbers = range(0, n)
 
+length = 50	
+angle = 90
+numbers = range(0, 100, 2)
+colors = ["blue", "red", "yellow", "green"]
 
 for number in numbers:
-	
-	carlos.forward(length)
-	carlos.left(angle)
-	length = length + 2
-	if length > 150:
-		length = 0
+	for color in colors:
+		carlos.color(color)
+		carlos.forward(length)
+		carlos.left(angle)
+		length = length + 2
 		
 	
 	
