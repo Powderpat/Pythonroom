d1731976422f630261b4bf9bd851b8ebec50c259
Powderpat <wpsawrey@gmail.com>
2014-04-25 17:17:34 -0700

    spiral

--- draw.py
+++ draw.py
@@ -5,8 +5,9 @@ import turtle
 carlos = turtle.Turtle()
 
 n = 50
-length = 100
-angle = 170
+t = 30
+length = 1	
+angle = 30
 numbers = range(0, n)
 
 
@@ -14,4 +15,9 @@ for number in numbers:
 	
 	carlos.forward(length)
 	carlos.left(angle)
+	length = length + 2
+	if length > 150:
+		length = 0
+		
+	
 	
